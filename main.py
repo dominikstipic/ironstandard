2687a5042e059b6ca68c12c59c631e32398b263d
dominik stipic <dominikstipic@gmail.com>
2024-08-28 15:47:30 +0200

    login

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request
 from waitress import serve
 
 
@@ -10,9 +10,20 @@ logging.getLogger('werkzeug').disabled = True
 
 #######################################
 
-@app.route("/")
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/", methods=['POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    if username == "dstipic" and password == "123":
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
+
 
 @app.route("/founder")
 def founder():
@@ -25,5 +36,5 @@ def demo():
 
 if __name__ == "__main__":
     print("Starting server!")
-    serve(app, host="0.0.0.0", port=80)
-    #app.run(host="127.0.0.1", port=8080, debug=True)
+    #erve(app, host="0.0.0.0", port=80)
+    app.run(host="127.0.0.1", port=8080, debug=True)
